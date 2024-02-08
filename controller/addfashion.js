2f79493720472e2cedaf4f9581aa907225014841
Athult7 <athultnair0777@gamil.com>
2024-02-08 21:37:51 +0530

    search

--- controller/addfashion.js
+++ controller/addfashion.js
@@ -17,4 +17,10 @@ router.get("/view",async(req,res)=>{
     let data=await fashionmodel.find()
     res.json(data)
 })
+
+router.post("/search",async(req,res)=>{
+    let input=req.body
+    let data=await fashionmodel.find(input)
+    res.json(data)
+})
 module.exports=router
